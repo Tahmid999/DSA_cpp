82cbf5f8b94ff499112a61e376bd9aaae73d9ab7
Tahmidur Rahman Muhtadee <80316577+Tahmid999@users.noreply.github.com>
2024-09-26 13:32:48 +0600

    26/9/24

---
+++ problems/leetcode/231_set_bit_method.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int count = 0;
+        if(n<0){
+            return false;
+        }
+        while(n!=0){
+            int bit = n&1;
+            if(bit == 1){
+                count++;
+            }
+            n = n >>1;
+        }
+        if(count==1){
+            return true;
+        }
+        else
+            return false;
+    }
+};
