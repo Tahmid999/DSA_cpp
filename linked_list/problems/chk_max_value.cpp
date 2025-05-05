02b0b89a8e8130baaafcbe73dab5e587df209b67
Tahmidur Rahman Muhtadee <80316577+Tahmid999@users.noreply.github.com>
2025-05-05 12:43:04 +0600

    Create chk_max_value.cpp

---
+++ linked_list/problems/chk_max_value.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node *next;
+        Node(int value)
+        {
+            this->value = value;
+            this->next = NULL;
+        }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node *new_node = new Node(val);
+
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    tail = new_node;
+    
+}
+
+void print_list(Node* temp)
+{
+    while (temp != NULL)
+    {
+        cout<<temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void chk_max(Node *temp)
+{
+    int max = INT_MIN;
+    while (temp != NULL)
+    {
+        if (temp->value > max)
+        {
+            max = temp->value;
+        }
+        temp = temp->next;
+    }
+    cout<<max<<endl;
+}
+
+int main() {
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(true)
+    {
+        int x;
+        cin>>x;
+        if (x == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, x);
+    }
+    chk_max(head);
+
+    return 0;
+}
